364c110364f2ba9694053556b6755134843e7496
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-29 23:10:00 +0200

    Initial TDD with adding some helpers

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication, RefreshDatabase;
+
+    public function signIn($user)
+    {
+        $user = $user ?: create('App\User');
+        $this->actingAs($user);
+
+        return $this;
+    }
+}
---
+++ tests/utilities/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+if(! function_exists('create')){
+    function create($model, $data = [], $count = null)
+    {
+        return factory($model, $count)->create($data);
+    }
+}
+
+if(! function_exists('make')){
+    function make($model, $data = [], $count = null)
+    {
+        return factory($model, $count)->create($data);
+    }
+}
