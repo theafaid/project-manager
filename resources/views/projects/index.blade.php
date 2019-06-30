459f2fd8f46ae32537a2ba4fc3c90a544ec29448
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-06-30 14:57:55 +0200

    Init Design

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,1 +1,35 @@
-{{$projects}}
+@extends('layouts.app')
+
+@section('content')
+    <div class="columns">
+        @foreach($projects as $project)
+            <div class="column is-half">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Component
+                        </p>
+                        <a href="#" class="card-header-icon" aria-label="more options">
+                      <span class="icon">
+                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                      </span>
+                        </a>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+                            <br>
+                            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="#" class="card-footer-item">Save</a>
+                        <a href="#" class="card-footer-item">Edit</a>
+                        <a href="#" class="card-footer-item">Delete</a>
+                    </footer>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
